2035bca5563dd8d6e795effe3ac4f87ebde1068d
Olexandr75 <sasha750505@gmail,com>
2023-05-26 12:32:54 +0300

    est

--- src/test/java/Lesson7/suite/Ex1.java
+++ src/test/java/Lesson7/suite/Ex1.java
@@ -1,4 +1,4 @@
-package lesson7.suite;
+package Lesson7.suite;
 
 import lesson5.MyWaiters;
 import org.openqa.selenium.By;
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.testng.Assert.*;
 import static org.testng.Assert.assertEquals;
 
-public class Ex1 extends BaseTest{
+public class Ex1 extends lesson7.suite.BaseTest {
 
 
     @Test
