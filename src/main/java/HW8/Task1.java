0e8331dcd690030920709134d399f2c4352a7a29
Olexandr75 <sasha750505@gmail,com>
2023-05-30 19:57:24 +0300

    HW8

--- src/main/java/HW8/Task1.java
+++ src/main/java/HW8/Task1.java
@@ -1,26 +1,26 @@
 package HW8;
 
-
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.function.Function;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 
 
    public class Task1 {
 
 
-        private final WebDriver driver;
 
-        //private WebDriverWait wait;//
-        public Task1(WebDriver driver){//int timeoutInSeconds//){
 
-            this.driver = driver;}
+       private final WebDriver driver;
+
+       public Task1(WebDriver driver) {
+           this.driver = driver;
+       }
 
 
        private static long EXPLICITY_WAIT=20L;
@@ -39,7 +39,7 @@ import java.util.function.Function;
             wait.until(function);
         }
 
-       /* public WebElement presenceOfElementLocated(By locator) {
+       public WebElement presenceOfElementLocated(By locator) {
             return fluentWait(EXPLICITY_WAIT).until(ExpectedConditions.presenceOfElementLocated(locator));
         }
 
@@ -93,40 +93,40 @@ import java.util.function.Function;
         }
 
         public void elementToBeSelected(String locator) {
-            wait.until(ExpectedConditions.elementToBeSelected(By.xpath(locator)));
+            fluentWait(EXPLICITY_WAIT).until(ExpectedConditions.elementToBeSelected(By.xpath(locator)));
         }
 
         public void elementSelectionStateToBe(WebElement element, boolean selected) {
-            wait.until(ExpectedConditions.elementSelectionStateToBe(element, selected));
+            fluentWait(EXPLICITY_WAIT).until(ExpectedConditions.elementSelectionStateToBe(element, selected));
         }
 
         public void elementSelectionStateToBe(By by, boolean selected) {
-            wait.until(ExpectedConditions.elementSelectionStateToBe(by, selected));
+            fluentWait(EXPLICITY_WAIT).until(ExpectedConditions.elementSelectionStateToBe(by, selected));
         }
 
         public void elementSelectionStateToBe(String locator, boolean selected) {
-            wait.until(ExpectedConditions.elementSelectionStateToBe(By.xpath(locator), selected));
+            fluentWait(EXPLICITY_WAIT).until(ExpectedConditions.elementSelectionStateToBe(By.xpath(locator), selected));
         }
 
         public WebElement visibilityOfElementLocated(By locator) {
-            return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            return fluentWait(EXPLICITY_WAIT).until(ExpectedConditions.visibilityOfElementLocated(locator));
         }
 
         public WebElement visibilityOfElementLocated(String locator) {
-            return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+            return fluentWait(EXPLICITY_WAIT).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
         }
 
         public void frameToBeAvailableAndSwitchToIt(By locator) {
-            wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(locator));
+            fluentWait(EXPLICITY_WAIT).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(locator));
         }
 
         public void frameToBeAvailableAndSwitchToIt(String locator) {
-            wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath(locator)));
+            fluentWait(EXPLICITY_WAIT).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath(locator)));
         }
 
         public void alertIsPresent() {
-            wait.until(ExpectedConditions.alertIsPresent());
-        }*/
+            fluentWait(EXPLICITY_WAIT).until(ExpectedConditions.alertIsPresent());
+        }
 
 
     }
